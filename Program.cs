535f5213d4710b9ce6b28a29de59153d7be66bab
FMorales <145341603+FMoranoALTAIR@users.noreply.github.com>
2023-10-04 11:50:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool salir = false;
+
+            while (!salir)
+            {
+
+                try
+                {
+
+                    Console.WriteLine("1. Opción 1");
+                    Console.WriteLine("2. Opción 2");
+                    Console.WriteLine("3. Cerrar");
+                    Console.WriteLine("Elige una de las opciones");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Has elegido la opción 1");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Has elegido la opción 2");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Has elegido cerrar de la aplicación");
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Elige una opcion entre 1 y 3");
+                            break;
+                    }
+
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
